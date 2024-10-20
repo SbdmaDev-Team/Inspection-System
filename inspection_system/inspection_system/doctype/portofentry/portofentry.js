220b0c239f3f64ae43bf63193ecd78425dc9feb7
Yousif A. Alhaj <yalhag@gmail.com>
2024-10-20 23:18:42 +0530

    T X

---
+++ inspection_system/inspection_system/doctype/portofentry/portofentry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, amr + yousif and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('PortOfEntry', {
+	// refresh: function(frm) {
+
+	// }
+});
