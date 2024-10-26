71cde026a2f98c7168c68fdb1fd65ecbbba25095
amratef84 <ammeratef84@gmail.com>
2024-10-26 19:41:34 +0530

    add report assignment

--- inspection_system/inspection_system/doctype/assignment/assignment.js
+++ inspection_system/inspection_system/doctype/assignment/assignment.js
@@ -2,7 +2,35 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Assignment', {
-	// refresh: function(frm) {
-
-	// }
-});
+	refresh(frm) {
+        frm.toggle_display(['data'], frm.doc.workflow_state === 'In Progress');
+        if (frm.doc.workflow_state == "In Progress"){
+            frm.add_custom_button(__('Create Assignment Report'), function() {			
+				frappe.model.with_doctype('AssignmentReport', function() {
+					var new_doc = frappe.model.get_new_doc('AssignmentReport');
+					new_doc.assignment = frm.doc.name;
+					console.log(new_doc);
+					console.log(frm.doc);
+					// فتح المستند الجديد
+					frappe.set_route('Form', 'AssignmentReport', new_doc.name);
+				});
+            }); // هذا يضيف الزر في قائمة الإجراءات
+           
+		}
+		if (frm.doc.creatingdate && frm.doc.duration) {
+            // حساب الفرق بين تاريخ اليوم وتاريخ الإنشاء
+            let creation_date = new Date(frm.doc.creatingdate);
+            let current_date = new Date();
+            
+            // حساب الفرق بالأيام بين اليوم وتاريخ الإنشاء
+            let time_diff = current_date - creation_date;
+            let diff_days = Math.floor(time_diff / (1000 * 60 * 60 * 24));
+            
+            // حساب التأخير
+            let delay = diff_days - frm.doc.duration;
+            
+            // إذا كان هناك تأخير (القيمة موجبة)
+            frm.set_value('delay_days', delay > 0 ? delay : 0);
+        }
+	}
+})
--- inspection_system/inspection_system/doctype/assignmentreport/assignmentreport.js
+++ inspection_system/inspection_system/doctype/assignmentreport/assignmentreport.js
@@ -2,7 +2,31 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('AssignmentReport', {
-	// refresh: function(frm) {
-
-	// }
+	 refresh: function(frm) {
+		console.log(frm.doc);
+		
+	 },
+	 before_save: function(frm) {
+		console.log(frm.doc);
+		if (frm.doc.assignment) {
+            frappe.call({
+                method: 'frappe.client.set_value',
+                args: {
+                    doctype: 'Assignment',    // نوع المستند الأصلي
+                    name: frm.doc.Assignment, // معرف المستند الأصلي
+                    fieldname: 'workflow_state',       // الحقل الذي تريد تحديثه
+                    value: 'Pending Approval'  // الحالة الجديدة التي تريد تعيينها
+                },
+                callback: function(response) {
+                    if (!response.exc) {
+                        frappe.msgprint(__('Assignment status updated successfully.'));
+                    } else {
+                        frappe.msgprint(__('Failed to update Assignment status.'));
+                    }
+                }
+            });
+        } else {
+            frappe.msgprint(__('Assignment is not Uplouded Report.'));
+        }	
+	 }
 });
